4c472a1faa8b43437848178509c5afd69d7ed1f1
Stephen Lenane <slenane1@gmail.com>
2020-09-06 21:21:33 -0400

    Added goal types and days since you quit

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 // DATA CONTROLLER
 let dataController = (function () {
-    let Goal = function (id, goal) {
-        this.id = id;
-        this.goal = goal;
-        this.percentage = -1;
+    class Goal {
+        constructor(id, goal) {
+            this.id = id;
+            this.goal = goal;
+            this.percentage = -1;
+        }
+    }
+
+    class Category extends Goal {
+        constructor(id, goal, subgoal) {
+            super(id, goal);
+            this.subgoal = subgoal;
+            this.percentage = -1;
+        }
+    }
+
+    class Quit extends Goal {
+        constructor(id, goal, date) {
+            super(id, goal);
+            this.date = date;
+            this.percentage = -1;
+        }
+    }
+
+    let allGoals = {
+        goalType: {
+            goal: [],
+            quit: [],
+        },
     };
 
-    let allGoals = [];
-
     return {
-        addGoal: function (goal) {
+        addGoal: function (type, goal, date) {
             let newGoal, ID;
 
             // Create an ID for the goal
@@ -18,10 +41,14 @@ let dataController = (function () {
             } else {
                 ID = 0;
             }
-            // Create new goal
-            newGoal = new Goal(ID, goal);
+            // Create new goal based on type
+            if (type === "goal") {
+                newGoal = new Category(ID, goal);
+            } else if (type === "quit") {
+                newGoal = new Quit(ID, goal, date);
+            }
             // Push new goal into goals array
-            allGoals.push(newGoal);
+            allGoals.goalType[type].push(newGoal);
             // Return the new goal
             return newGoal;
         },
@@ -31,7 +58,9 @@ let dataController = (function () {
 // UI CONTROLLER
 let UIController = (function () {
     let DOMstrings = {
+        goalType: ".add_goal_type",
         goalInput: ".add_goal_input",
+        goalDate: ".add_goal_date",
         goalSubmit: ".add_goal_button",
         goalsList: ".goals",
         goalItem: ".grid-item",
@@ -48,21 +77,35 @@ let UIController = (function () {
     return {
         getinput: function () {
             return {
+                type: document.querySelector(DOMstrings.goalType).value,
                 goal: document.querySelector(DOMstrings.goalInput).value,
+                date: document.querySelector(DOMstrings.goalDate).value,
             };
         },
 
-        addListItem: function (obj) {
+        addListItem: function (obj, type, date) {
             let element, html, newHtml;
+            let goalDate = date;
             // Create HTML string with placeholder text
-            element = DOMstrings.goalsList;
-            html =
-                '<div class="grid-item" id="goal-%id%"><h2 class="goal-title">%title%</h2><img class="percentage-wheel" src="./percentage-sample.jpg"alt="percentage wheel"/></div>';
+            if (type === "goal") {
+                element = DOMstrings.goalsList;
+                html =
+                    '<div class="grid-item goal-item" id="goal-%id%"><h2 class="goal-title">%title%</h2><img class="percentage-wheel" src="./percentage-sample.jpg"alt="percentage wheel"/></div>';
+            } else if (type === "quit") {
+                element = DOMstrings.goalsList;
+                html =
+                    '<div class="grid-item quit-item" id="goal-%id%"><h2 class="goal-title">%title%</h2><p>%date% days</p></div>';
+            }
 
             // Replace placeholder text with actual data
             newHtml = html.replace("%id%", obj.id);
             newHtml = newHtml.replace("%title%", obj.goal);
-
+            newHtml = newHtml.replace(
+                "%date%",
+                Math.floor(
+                    (new Date().getTime() - goalDate) / (60 * 60 * 24 * 1000)
+                )
+            );
             // Insert into the DOM
             document
                 .querySelector(element)
@@ -129,17 +172,36 @@ let controller = (function (dataCtrl, UICtrl) {
     };
 
     let ctrlAddGoal = function () {
-        let input, newGoal;
+        let input, newGoal, date, newDate;
         // Get input from user
         input = UICtrl.getinput();
-        // If the filed is not empty
-        if (input.goal !== "") {
+
+        // If the type is goal and the field is not empty
+        if (input.type === "goal" && input.goal !== "") {
             // Add the goal to the data controller
-            newGoal = dataCtrl.addGoal(input.goal);
+            newGoal = dataCtrl.addGoal(input.type, input.goal);
 
             // Remove add goal message and add the goal to the UI
             UICtrl.hideMessage();
-            UICtrl.addListItem(newGoal);
+            UICtrl.addListItem(newGoal, input.type);
+
+            // Clear the input field
+            UICtrl.clearFields();
+        }
+
+        // If the type is quit and the field/date are not empty
+        if (input.type === "quit" && input.goal !== "" && input.date !== "") {
+            // Convert date into timestamp
+            date = input.date.split("-");
+            newDate = new Date(date[0], date[1] - 1, date[2]);
+            newDate = newDate.getTime();
+
+            // Add the goal to the data controller
+            newGoal = dataCtrl.addGoal(input.type, input.goal, newDate);
+
+            // Remove add goal message and add the goal to the UI
+            UICtrl.hideMessage();
+            UICtrl.addListItem(newGoal, input.type, newDate);
 
             // Clear the input field
             UICtrl.clearFields();
